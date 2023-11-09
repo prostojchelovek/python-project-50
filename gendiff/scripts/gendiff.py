baddadcf01b1b6c66939c3f4707bc01160f03022
alaev <alaev.musost@mail.ru>
2023-11-09 03:48:23 +0300

    add argument format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,11 +4,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('file', metavar='first_file')
-    parser.add_argument('file', metavar='second_file')
+    parser.add_argument('file1', metavar='first_file')
+    parser.add_argument('file2', metavar='second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
